2d246c9da2c65379e4e93778555601fd5a8e6f2b
Alessandra Duque <alessandra_1804@hotmail.com>
2022-10-14 20:30:21 -0300

    ll

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create!(
+  name: "Huengry",
+  address: "Rua Geraldo Pinheiro, 173",
+  phone_number: "(11)99132-3021",
+  category: "belgian"
+)
+
+Restaurant.create!(
+  name: "Le Roux",
+  address: "Rua Alto Coqueiro, 24",
+  phone_number: "(11) 99384-3294",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Mamma Pasta",
+  address: "Rua Franco Ernesto, 987",
+  phone_number: "(11) 99923-5098",
+  category: "italian"
+)
+
+Restaurant.create!(
+  name: "Toponaki",
+  address: "Rua Costa Silva, 19",
+  phone_number: "(11) 99707-6665",
+  category: "japanese"
+)
+
+Restaurant.create!(
+  name: "Mig Mig",
+  address: "Rua Doro Verde, 400",
+  phone_number: "(11) 99290-8792",
+  category: "chinese"
+)
